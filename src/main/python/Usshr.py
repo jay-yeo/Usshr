fc11887b7afdb562f72f84c96d2e8827951fcb2e
Jason Yeo <jayyeo21@gmail.com>
2018-04-29 02:49:45 +0200

    Created classes for Connection, Messenger and Player

--- src/main/python/Usshr.py
+++ src/main/python/Usshr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Usshr - A simple script to play media on the RaspberryPi over SSH.
-# By Jay Yeo - github.com/jay-yeo
+# Credits: Jay Yeo - github.com/jay-yeo
 
 import os
 import paramiko
@@ -15,115 +15,145 @@ class Connection:
         self.user = username
         self.pwd = password
 
+# SSH Messaging Functions Class
+class Messenger:
 
-# SSH Message Transport
-def sshMessenger(Connection, message, reply):
+    # Constructor
+    def __init__(self, Connection):
+        self.Connection = Connection
 
-    try:
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.WarningPolicy)
+    # Send message
+    def send(self, message):
 
-        client.connect(Connection.host, port=Connection.port, username=Connection.user, password=Connection.pwd)
+        try:
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+            client.set_missing_host_key_policy(paramiko.WarningPolicy)
 
-        # Send SSH Command
-        stdin, stdout, stderr = client.exec_command(message)
+            client.connect(self.Connection.host, port=self.Connection.port, username=self.Connection.user, password=self.Connection.pwd)
 
-        # Terminal Reply
-        if reply == True:
+            # Send SSH Command
+            stdin, stdout, stderr = client.exec_command(message)
+
+            print('Command Sent!')
+        except:
+            print('Fuck!')
+
+        client.close()
+
+    # Send message and return reply
+    def sendReply(self, message):
+
+        try:
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+            client.set_missing_host_key_policy(paramiko.WarningPolicy)
+
+            client.connect(self.Connection.host, port=self.Connection.port, username=self.Connection.user, password=self.Connection.pwd)
+
+            # Send SSH Command
+            stdin, stdout, stderr = client.exec_command(message)
             reply = stdout.readlines()
+
+            print('Command Sent!')
             return reply
+        except:
+            print('Fuck!')
 
-        print('Command Sent!')
-    except:
-        print('Fuck!')
-
-    client.close()
+        client.close()
 
 
-# Play Function
-def play(video):
+# Player Class
+class Player:
 
-    video_dir = '/media/pi/VIDEO/YouTube/'
-    video_url = video_dir + video
+    # Constructor
+    def __init__(self, Connection):
+        # Connection Details
+        self.cnx = Connection
+        self.messenger = Messenger(self.cnx)
 
-    play_string = 'omxplayer --no-keys -o hdmi -b ' + '"' + str(video_url) + '" &'
-    return sshMessenger(play_string,reply=False)
 
-# Terminal Playback Controls
-def playbackCTRL():
+    # Play Function
+    def play(self, videoURL):
 
-    # Give the user some context.
-    print('------------------')
-    print('Playback Controls:')
+        #play_string = 'omxplayer --no-keys -o hdmi -b ' + '"' + str(videoURL) + '" &'
+        self.messenger.send(videoURL)
+        self.playbackControls()
 
-    # Set an initial value for choice other than the value for 'quit'.
-    command = ''
+    # Terminal Playback Controls
+    def playbackControls(self):
 
-    # Start a loop that runs until the user enters the value for 'quit'.
-    while command != 'q':
+        # Give the user some context.
+        print('------------------')
+        print('Playback Controls:')
 
-        # Command choices
-        print('[p] Pause')
-        print('[q] Quit')
+        # Set an initial value for choice other than the value for 'quit'.
+        command = ""
 
-        # Ask for user choice
-        command = raw_input('Command: ')
+        # Start a loop that runs until the user enters the value for 'quit'.
+        while command != "q":
 
-        # Respond to user choice
-        if command == 'p':
-            sshMessenger('dbuscontrol.sh pause', reply=False)
-            print('Pausing Playback')
-        elif command == 'q':
-            sshMessenger('pkill omxplayer',reply=False)
-            print('Goodbye!')
-        else:
-            print('Incorrect Command')
+            # Command choices
+            print("[p] Pause")
+            print("[q] Quit")
+
+            # Ask for user choice
+            command = input("Command: ")
+
+            # Respond to user choice
+            if command == "p":
+                self.messenger.send("dbuscontrol.sh pause")
+                print("Pausing Playback")
+            elif command == "q":
+                self.messenger.send("pkill omxplayer")
+                print("Goodbye!")
+            else:
+                print("Incorrect Command")
 
 #Run Script
-if __name__ == '__main__':
-    print('PiTube - Theatre')
+if __name__ == "__main__":
+    print("Usshr - Theatre")
 
-    # CONFIG - Add your details
-    host = "localhost"
+    # CONFIG - Add your device authorization details
+    host = "192.168.0.87"
     port = 22
-    user = "yourUsername"
-    password = "yourPassword"
+    user = "pi"
+    password = "Budap3st1!"
 
-    cxDetails = Connection(host, port, user, password)
+    piConnection = Connection(host, port, user, password)
 
-    command = ''
+    Player(piConnection).play("video")
 
     # Start a loop that runs until the user enters the value for 'quit'.
-    while command != 'q':
-
-        # Give all the choices in a series of print statements.
-        print('[x] Start Projector')
-        print('[l] List Movies')
-        print('[q] Quit')
-
-        # Ask for the user's choice.
-        command = raw_input('Command: ')
-
-        # List Movies
-        if command == 'l':
-            list_string = "find *.mkv *.mp4 /media/pi/VIDEO/ "
-            movie_list = sshMessenger(cxDetails, list_string,reply=True)
-            print('All Movies:')
-            if '.mkv' or '.mp4' in movie_list:
-            for movie in movie_list:
-                print(movie)
-            print(movie_list)
-        # Play Movies
-        elif command == 'x':
-            movie = raw_input('Enter Movie: ')
-            print('Starting "' + str(movie) + '"')
-            play(movie)
-            playbackCTRL()
-        # Quit
-        elif command == 'q':
-            print('Goodbye!')
-            break
-        # Incorrect Command
-        else:
-            print('Incorrect Command')
+    # while command != 'q':
+    #
+    #     # Give all the choices in a series of print statements.
+    #     print('[x] Start Projector')
+    #     print('[l] List Movies')
+    #     print('[q] Quit')
+    #
+    #     # Ask for the user's choice.
+    #     command = input('Command: ')
+    #
+    #     # List Movies
+    #     if command == 'l':
+    #         list_string = "find *.mkv *.mp4 /media/pi/VIDEO/ "
+    #         movie_list = sshMessenger(cxDetails, list_string,reply=True)
+    #         print('All Movies:')
+    #         if '.mkv' or '.mp4' in movie_list:
+    #         for movie in movie_list:
+    #             print(movie)
+    #         print(movie_list)
+    #     # Play Movies
+    #     elif command == 'x':
+    #         movie = raw_input('Enter Movie: ')
+    #         print('Starting "' + str(movie) + '"')
+    #         play(movie)
+    #         playbackCTRL()
+    #     # Quit
+    #     elif command == 'q':
+    #         print('Goodbye!')
+    #         break
+    #     # Incorrect Command
+    #     else:
+    #         print('Incorrect Command')
